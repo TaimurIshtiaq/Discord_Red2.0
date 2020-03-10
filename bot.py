59d5ec82e91848ec4f589c3f307843fb08c604c9
TaimurIshtiaq <56674122+TaimurIshtiaq@users.noreply.github.com>
2020-03-10 18:08:48 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,4 +53,4 @@ async def on_message(message):
         await message.channel.send(response)
 
 
-client.run('Njg2MjI5NTM5NzA5NjQ4OTU2.XmeNwA.cfU8X6j5oCK9I1PghpXw2maY2mw')
+client.run('Njg2MjI5NTM5NzA5NjQ4OTU2.XmeROA.GBI5hI-ck0FVf1n65NMs3Ef6KD8')
